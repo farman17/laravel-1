a28fc5ab46d918e82be2778419bd1a56d012fcea
farman17 <69154746+farman17@users.noreply.github.com>
2020-10-11 11:16:43 +0700

    ubah content main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <i>Farman</i> Website for Laravel
                 </div>
 
                 <div class="links">
